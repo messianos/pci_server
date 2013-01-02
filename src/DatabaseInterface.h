4e578414fe3925eb7f43f70378cb985b895b2838
messianos <messianosuns@gmail.com>
2013-01-02 21:03:59 -0200

    Update src/DatabaseInterface.h

--- src/DatabaseInterface.h
+++ src/DatabaseInterface.h
@@ -1,25 +1,38 @@
+
+
 #ifndef DATABASEINTERFACE_H_
 #define DATABASEINTERFACE_H_
 
+#include "Problem.h"
+#include "Solution.h"
+#include "Clarification.h"
+#include "User.h"
+#include <cppdb/frontend.h>
+#include <list>
+
 class DatabaseInterface {
+private:
+	cppdb::session databaseHandler;
+	void connectToDatabase();
+
 public:
 	DatabaseInterface();
 	virtual ~DatabaseInterface();
 
 	// Problem-related queries
-	Problem getProblem(long id);
-	/* TODO: define returned data structure */ searchProblems(/* TODO: define parameters */);
+	Problem *getProblem(long id);
+	list<Problem> searchProblems(/* TODO: define parameters */);
 
 	// Solution-related queries
-	Solution getSolution(long id);
-	/* TODO: define returned data structure */ searchSolutions(long problem_id);
+	Solution *getSolution(long id);
+	list<Solution> searchSolutions(long problem_id);
 
 	// Clarification-related queries
-	Clarification getClarification(long id);
-	/* TODO: define returned data structure */ searchClarifications(long associated_publication_id);
+	Clarification *getClarification(long id);
+	list<Clarification> searchClarifications(long associated_publication_id);
 
 	// User-related queries
-	User getUser(long user_name);
+	User *getUser(long user_name);
 };
 
 #endif /* DATABASEINTERFACE_H_ */
