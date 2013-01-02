ae3b139f05c6f1be7f236ddac78dc05d44490007
messianos <messianosuns@gmail.com>
2013-01-02 17:14:06 -0300

    Publication added

---
+++ src/Publication.cpp
@@ -0,0 +1,24 @@
+/*
+ * Publication.cpp
+ *
+ *  Created on: 02/01/2013
+ *      Author: nico
+ */
+
+#include "Publication.h"
+
+Publication::Publication(long id, string content, string description,
+		bool is_anonymous, time_t last_edition_datetime,
+		time_t creation_datetime) {
+	this->id = id;
+	this->content = content;
+	this->description = description;
+	this->is_anonymous = is_anonymous;
+	this->last_edition_datetime = last_edition_datetime;
+	this->creation_datetime = creation_datetime;
+}
+
+Publication::~Publication() {
+// TODO Auto-generated destructor stub
+}
+
---
+++ src/Publication.h
@@ -0,0 +1,30 @@
+/*
+ * Publication.h
+ *
+ *  Created on: 02/01/2013
+ *      Author: nico
+ */
+
+#include <string>
+#include <ctime>
+using namespace std;
+
+#ifndef PUBLICATION_H_
+#define PUBLICATION_H_
+
+class Publication {
+public:
+	Publication(long id, string content, string description,
+			bool is_anonymous, time_t last_edition_datetime,
+			time_t creation_datetime);
+	virtual ~Publication();
+private:
+	long id;
+	string content;
+	string description;
+	bool is_anonymous;
+	time_t last_edition_datetime;
+	time_t creation_datetime;
+};
+
+#endif /* PUBLICATION_H_ */
