dd405b6396687e3c068f63742dc6b6e862bb970b
messianos <messianosuns@gmail.com>
2013-01-02 17:58:57 -0300

    Fede

--- src/main_screen_content.h
+++ src/main_screen_content.h
@@ -2,8 +2,6 @@
 #include <string>
 using namespace std;
 
-// Comentario de Fede
-//comentario de Gonza
 namespace main_screen_content{
 	struct content: public cppcms::base_content{
 		string user_id;
