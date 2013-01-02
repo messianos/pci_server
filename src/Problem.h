5977e1624c859194dceca49c2eb7416a077a0cae
messianos <messianosuns@gmail.com>
2013-01-02 17:36:30 -0300

    commit

---
+++ src/Problem.h
@@ -0,0 +1,19 @@
+/*
+ * Problem.h
+ *
+ *  Created on: 02/01/2013
+ *      Author: nico
+ */
+
+#ifndef PROBLEM_H_
+#define PROBLEM_H_
+
+#include "Publication.h"
+
+class Problem : public Publication {
+public:
+	Problem();
+	virtual ~Problem();
+};
+
+#endif /* PROBLEM_H_ */
