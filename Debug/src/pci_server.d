6250ec65abe1e1de998db60b8cb7e4978c5703f3
messianos <messianosuns@gmail.com>
2013-01-02 17:53:44 -0300

    hola

--- Debug/src/pci_server.d
+++
@@ -1,3 +0,0 @@
-src/pci_server.d: ../src/pci_server.cpp ../src/main_screen_content.h
-
-../src/main_screen_content.h:
--- Debug/src/pci_template.d
+++
@@ -1,3 +0,0 @@
-src/pci_template.d: ../src/pci_template.cpp ../src/main_screen_content.h
-
-../src/main_screen_content.h:
