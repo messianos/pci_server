5fa62ebb0f771cd1007e2af6815135dbc4f0a53b
messianos <messianosuns@gmail.com>
2013-01-02 19:02:37 -0300

    Fede

--- src/DatabaseInterface.cpp
+++ src/DatabaseInterface.cpp
@@ -1,18 +1,63 @@
-/*
- * DatabaseInterface.cpp
- *
- *  Created on: Jan 2, 2013
- *      Author: fede
- */
-
 #include "DatabaseInterface.h"
 
-DatabaseInterface::DatabaseInterface() {
-	// TODO Auto-generated constructor stub
+void DatabaseInterface::connectToDatabase() {
+	databaseHandler = cppdb::session("mysql: database=pci_database; user=pci_user; password='pci_password'");
+}
 
+DatabaseInterface::DatabaseInterface() {
+	connectToDatabase();
 }
 
 DatabaseInterface::~DatabaseInterface() {
-	// TODO Auto-generated destructor stub
+	// TODO Auto-generated destructor stub}
+	databaseHandler.~session();
 }
 
+Problem DatabaseInterface::getProblem(long id) {
+
+	Problem problem;
+
+	cppdb::result result = databaseHandler << "SELECT * FROM Problem WHERE id = ?" << id;
+	problem = new Problem();
+	// TODO: set the problem attributes
+
+	return problem;
+}
+
+list<Problem> DatabaseInterface::searchProblems(/* TODO: define parameters */) {
+	// TODO
+}
+
+Solution DatabaseInterface::getSolution(long id) {
+
+	Solution solution;
+
+	cppdb::result result = databaseHandler << "SELECT * FROM Solution WHERE id = ?" << id;
+	solution = new Solution();
+	// TODO: set the solution attributes
+
+	return solution;
+}
+
+list<Solution> DatabaseInterface::searchSolutions(long problem_id) {
+	// TODO
+}
+
+Clarification DatabaseInterface::getClarification(long id) {
+
+	Clarification clarification;
+
+	cppdb::result result = databaseHandler << "SELECT * FROM Clarification WHERE id = ?" << id;
+	clarification = new Clarification();
+	// TODO: set the clarification attributes
+
+	return clarification;
+}
+
+list<Clarification> DatabaseInterface::searchClarifications(long associated_publication_id) {
+	// TODO
+}
+
+User DatabaseInterface::getUser(long user_name) {
+	// TODO
+}
--- src/DatabaseInterface.h
+++ src/DatabaseInterface.h
@@ -7,23 +7,29 @@
 #include "Solution.h"
 #include "Clarification.h"
 #include "User.h"
+#include <cppdb/frontend.h>
+#include <list>
 
 class DatabaseInterface {
+private:
+	cppdb::session databaseHandler;
+	void connectToDatabase();
+
 public:
 	DatabaseInterface();
 	virtual ~DatabaseInterface();
 
 	// Problem-related queries
 	Problem getProblem(long id);
-	/* TODO: define returned data structure */ searchProblems(/* TODO: define parameters */);
+	list<Problem> searchProblems(/* TODO: define parameters */);
 
 	// Solution-related queries
 	Solution getSolution(long id);
-	/* TODO: define returned data structure */ searchSolutions(long problem_id);
+	list<Solution> searchSolutions(long problem_id);
 
 	// Clarification-related queries
 	Clarification getClarification(long id);
-	/* TODO: define returned data structure */ searchClarifications(long associated_publication_id);
+	list<Clarification> searchClarifications(long associated_publication_id);
 
 	// User-related queries
 	User getUser(long user_name);
