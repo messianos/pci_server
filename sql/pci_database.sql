55af4d4b4147ed49034c7cb099ddcee5873aafb7
messianos <messianosuns@gmail.com>
2013-01-03 18:44:42 -0300

    Actualizacion de pci_database.sql

--- sql/pci_database.sql
+++ sql/pci_database.sql
@@ -1,70 +1,176 @@
 
--- Database creation
-CREATE DATABASE pci_database;
-USE pci_databse;
+-- DATABASE CREATION AND CONFIGURATION ----------------------------------------
+
+CREATE DATABASE IF NOT EXISTS pci_database
+CHARACTER SET utf8;
+
+USE pci_database;
 
 
--- Application user creation
-CREATE USER 'pci_user'@'localhost' IDENTIFIED BY 'pci_user';
+
+-- USERS ----------------------------------------------------------------------
+
+-- If the user doesn't exist, it is automatically created
+-- Note: NO_AUTO_CREATE_USER mode must be disabled
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON pci_database.*
+TO 'pci_user'@'localhost'
+IDENTIFIED BY PASSWORD '*6D4CC5751FA512A297F78B8A892AAF9D051B0231';
 
 
--- Table definitions
 
-CREATE TABLE Clarification(
-	answer VARCHAR(MAX_CHAR_ANSWER),
-	associated_publication_id TIPODEID NOT NULL,
-	creator_user_name VARCHAR(MAX_CHAR_USER_NAME) NOT NULL,
-	id TIPODEID NOT NULL,
-	question VARCHAR(MAX_CHAR_QUESTION) NOT NULL,
+-- TABLES ---------------------------------------------------------------------
+
+/*
+ * Storage requirements
+ * 
+ * VARCHAR(M) L+1 bytes if column values require 0 – 255 bytes
+ *            L+2 bytes if values may require more than 255 bytes
+ * 
+ * TINYTEXT   L+1 bytes, where L < 2^8  = 256 bytes
+ * TEXT       L+2 bytes, where L < 2^16 = 65.536 bytes = 64 KB
+ * MEDIUMTEXT L+3 bytes, where L < 2^24 = 16.777.216 bytes = 16 MB
+ * LONGTEXT   L+4 bytes, where L < 2^32 = 4.294.967.296 bytes = 4 GB
+ * 
+ * 
+ * Variable-length string types are stored using a length prefix plus data.
+ * The length prefix requires from one to three bytes depending on the data
+ * type, and the value of the prefix is L (the byte length of the string).
+ * 
+ * To calculate the number of bytes used to store a particular CHAR, VARCHAR,
+ * or TEXT column value, you must take into account the character set used
+ * for that column and whether the value contains multi-byte characters. In
+ * particular, when using the utf8 Unicode character set, you must keep in
+ * mind that not all characters use the same number of bytes and can require
+ * up to four bytes per character.
+ * 
+ * Each TEXT value is represented internally by a separately allocated
+ * object. This is in contrast to the VARCHAR data type, for which storage
+ * is allocated once per column when the table is opened.
+ * 
+ */
+
+/*
+
+CREATE TABLE IF NOT EXISTS Clarification (
+	answer TEXT,
+	associated_publication_id TIPODEID,
+	creator_user_name VARCHAR(MAX_CHAR_USER_NAME),
+	id TIPODEID,
+	question TEXT,
 	PRIMARY KEY(id),
 	FOREIGN KEY(associated_publication_id) REFERENCES Publication(id),
 	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
 );
 
-CREATE TABLE User(
-	birth_date DATE NOT NULL,
-email VARCHAR(MAX_CHAR_EMAIL) NOT NULL,
-	first_name VARCHAR(MAX_CHAR_FIRST_NAME) NOT NULL,
-genre CHAR(1) NOT NULL, # M: male / F: female / U: unspecified
-	last_name VARCHAR(MAX_CHAR_LAST_NAME) NOT NULL,
-	location TIPODELOCATION NOT NULL,
-	password TIPODEPASSWORD NOT NULL,
-	signup_date DATE NOT NULL,
-	user_name VARCHAR(MAX_CHAR_USER_NAME) NOT NULL,
+CREATE TABLE IF NOT EXISTS User (
+	birth_date DATE,
+	email VARCHAR(MAX_CHAR_EMAIL),
+	first_name VARCHAR(MAX_CHAR_FIRST_NAME),
+	genre ENUM('M', 'F', 'U'), -- M: male / F: female / U: unspecified
+	last_name VARCHAR(MAX_CHAR_LAST_NAME),
+	location TIPODELOCATION,
+	password VARCHAR(MAX_CHAR_PASSWORD), -- Encrypted password
+	signup_date DATE,
+	user_name VARCHAR(MAX_CHAR_USER_NAME),
 	PRIMARY KEY(user_name)
 );
 
-CREATE TABLE Problem(
-	accepted_solution_id TIPODEID NOT NULL,
-	content VARCHAR(MAX_CHAR_CONTENT),
+CREATE TABLE IF NOT EXISTS Problem (
+	accepted_solution_id TIPODEID,
+	content MEDIUMTEXT,
 	creation_datetime DATETIME,
-	creator_user_name VARCHAR(MAX_CHAR_USER_NAME) NOT NULL,
-	description VARCHAR(MAX_CHAR_DESCRIPTION) NOT NULL,
-	id TIPODEID NOT NULL,
-	is_anonymous BOOL NOT NULL,
-	is_solved BOOL NOT NULL,
-	last_edition_datetime DATETIME NOT NULL,
+	creator_user_name VARCHAR(MAX_CHAR_USER_NAME),
+	description TEXT,
+	id TIPODEID,
+	is_anonymous BOOLEAN,
+	is_solved BOOLEAN,
+	last_edition_datetime DATETIME,
 	PRIMARY KEY(id),
 	FOREIGN KEY(accepted_solution_id) REFERENCES Solution(id),
 	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
 );
 
-CREATE TABLE Solution(
-	content VARCHAR(MAX_CHAR_CONTENT),
+CREATE TABLE IF NOT EXISTS Solution (
+	content MEDIUMTEXT,
 	creation_datetime DATETIME,
-	creator_user_name VARCHAR(MAX_CHAR_USER_NAME) NOT NULL,
-	description VARCHAR(MAX_CHAR_DESCRIPTION) NOT NULL,
-	id TIPODEID NOT NULL,
-	is_anonymous BOOL NOT NULL,
-	last_edition_datetime DATETIME NOT NULL,
+	creator_user_name VARCHAR(MAX_CHAR_USER_NAME),
+	description TEXT,
+	id TIPODEID,
+	is_anonymous BOOLEAN,
+	last_edition_datetime DATETIME,
 	PRIMARY KEY(id),
 	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
 );
 
-CREATE TABLE problem_solutions(
-	problem_id TIPODEID NOT NULL,
-	solution_id TIPODEID NOT NULL,
+CREATE TABLE IF NOT EXISTS problem_solutions (
+	problem_id TIPODEID,
+	solution_id TIPODEID,
 	PRIMARY KEY(problem_id, solution_id),
 	FOREIGN KEY(problem_id) REFERENCES Problem(id),
 	FOREIGN KEY(solution_id) REFERENCES Solution(id)
 );
+
+*/
+
+CREATE TABLE IF NOT EXISTS User (
+	birth_date DATE,
+	email VARCHAR(255),
+	first_name VARCHAR(31),
+	genre ENUM('M', 'F', 'U'), -- M: male / F: female / U: unspecified
+	last_name VARCHAR(31),
+	location VARCHAR(15),
+	password VARCHAR(127), -- Encrypted password
+	signup_date DATE,
+	user_name VARCHAR(31),
+	PRIMARY KEY(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS Solution (
+	content MEDIUMTEXT,
+	creation_datetime DATETIME,
+	creator_user_name VARCHAR(31),
+	description TEXT,
+	id BIGINT,
+	is_anonymous BOOLEAN,
+	last_edition_datetime DATETIME,
+	PRIMARY KEY(id),
+	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS Problem (
+	accepted_solution_id BIGINT,
+	content MEDIUMTEXT,
+	creation_datetime DATETIME,
+	creator_user_name VARCHAR(31),
+	description TEXT,
+	id BIGINT,
+	is_anonymous BOOLEAN,
+	is_solved BOOLEAN,
+	last_edition_datetime DATETIME,
+	PRIMARY KEY(id),
+	FOREIGN KEY(accepted_solution_id) REFERENCES Solution(id),
+	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS Clarification (
+	answer TEXT,
+	associated_publication_id BIGINT,
+	creator_user_name VARCHAR(31),
+	id BIGINT,
+	question TEXT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(creator_user_name) REFERENCES User(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS problem_solutions (
+	problem_id BIGINT,
+	solution_id BIGINT,
+	PRIMARY KEY(problem_id, solution_id),
+	FOREIGN KEY(problem_id) REFERENCES Problem(id),
+	FOREIGN KEY(solution_id) REFERENCES Solution(id)
+);
+
+-- TESTING --------------------------------------------------------------------
+
+
