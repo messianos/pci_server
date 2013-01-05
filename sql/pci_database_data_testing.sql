6e900128202bf683cb97624b16ff4ca0524b3805
messianos <messianosuns@gmail.com>
2013-01-05 09:20:15 -0300

    Lalal Fede

--- sql/pci_database_data_testing.sql
+++ sql/pci_database_data_testing.sql
@@ -1,6 +1,13 @@
 
 USE pci_database;
 
+ALTER TABLE Problem MODIFY id BIGINT AUTO_INCREMENT;
+ALTER TABLE Problem AUTO_INCREMENT = 1000;
+ALTER TABLE Solution MODIFY id BIGINT AUTO_INCREMENT;
+ALTER TABLE Solution AUTO_INCREMENT = 0;
+ALTER TABLE Clarification MODIFY id BIGINT AUTO_INCREMENT;
+ALTER TABLE Clarification AUTO_INCREMENT = 2000;
+
 DELETE IGNORE FROM problem_solutions;
 DELETE IGNORE FROM Clarification;
 DELETE IGNORE FROM Problem;
@@ -12,14 +19,14 @@ VALUES ('problem_related_user', 'password');
 INSERT IGNORE INTO User(user_name, password)
 VALUES ('solution_related_user', 'password');
 
-INSERT IGNORE INTO Problem(content, creator_user_name, description, id, is_anonymous, is_solved)
-VALUES ('problem content', 'problem_related_user', 'problem description', 123, FALSE, FALSE);
-INSERT IGNORE INTO Solution(content, creator_user_name, description, id, is_anonymous)
-VALUES ('solution content', 'solution_related_user', 'solution description', 345, FALSE);
+INSERT IGNORE INTO Problem(content, creator_user_name, description, is_anonymous, is_solved)
+VALUES ('problem content', 'problem_related_user', 'problem description', FALSE, FALSE);
+INSERT IGNORE INTO Solution(content, creator_user_name, description, is_anonymous)
+VALUES ('solution content', 'solution_related_user', 'solution description', FALSE);
 INSERT IGNORE INTO problem_solutions(problem_id, solution_id)
-VALUES (123, 345);
+VALUES (1000, 1);
 
-INSERT IGNORE INTO Clarification(answer, associated_publication_id, creator_user_name, id, question)
-VALUES ('answer of problem_related_user', 123, 'solution_related_user', 67, 'question from solution_related_user');
-INSERT IGNORE INTO Clarification(answer, associated_publication_id, creator_user_name, id, question)
-VALUES ('answer of solution_related_user', 345, 'problem_related_user', 90, 'question from problem_related_user');
+INSERT IGNORE INTO Clarification(answer, associated_publication_id, creator_user_name, question)
+VALUES ('answer of problem_related_user', 1000, 'solution_related_user', 'question from solution_related_user');
+INSERT IGNORE INTO Clarification(answer, associated_publication_id, creator_user_name, question)
+VALUES ('answer of solution_related_user', 1, 'problem_related_user', 'question from problem_related_user');
